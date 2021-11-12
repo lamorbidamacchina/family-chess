58786dea92e9ee2ad64226eaedde17563e4e225b
Simone Ricci <simogol@gmail.com>
2021-11-12 18:47:21 +0100

    board status storage, reset

--- public/default.js
+++ public/default.js
@@ -3,7 +3,20 @@ var game;
 
 window.onload = function () {
     initGame();
+
+    // manage board flip
     $('#flipOrientationBtn').on('click', board.flip);
+
+    // manage board reset
+    function resetBoard(board) {
+        console.log("resetting board...");
+        board.start;
+        localStorage.setItem("fen","");  
+        location.reload(); // patch... non funziona board start
+      }
+    $('#startPositionBtn').on('click', resetBoard);
+
+    // fix drag n drop on mobile
     jQuery('#chess_board').on('scroll touchmove touchend touchstart contextmenu', function(e){
         e.preventDefault();
     });
@@ -17,11 +30,15 @@ window.onclick = function(e) {
     socket.emit('message', 'hello chess players!');
 };
 */
+var game_position = 'start';
+if (localStorage.getItem("fen")) {
+    game_position = localStorage.getItem("fen");
+}
 
 var initGame = function() {
    var cfg = {
        draggable: true,
-       position: 'start',
+       position: game_position,
        // orientation: 'black',
        onDrop: handleMove,
    };
@@ -43,9 +60,6 @@ var handleMove = function(source, target ) {
     }
 };
 
-
-
-
 // called when the server calls socket.broadcast('move')
 socket.on('move', function (msg) {
     game.move(msg);  
@@ -53,5 +67,6 @@ socket.on('move', function (msg) {
     console.log("recvrs: " + board.fen());
     document.getElementById("fen").innerHTML = board.fen();
     // TO DO - save data somewhere in local storage, to recover in case of connection lost
+    localStorage.setItem("fen", board.fen());
 });
 
